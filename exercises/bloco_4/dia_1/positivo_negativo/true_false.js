9ed9729dd14bcb3f765f81a1b1955304d7d21341
victor718 <victorfelipeoliveira1@gmail.com>
2020-10-23 18:10:36 -0300

    exerc_4 positivo ou negativo

---
+++ exercises/bloco_4/dia_1/positivo_negativo/true_false.js
@@ -0,0 +1,9 @@
+// Faça um programa que, dado um valor definido numa variável, 
+// retorne "positive" se esse valor for positivo,
+// "negative" se for negativo e "zero" caso contrário.
+
+let a = -4;
+
+let res = a >= 0 ? 'O valor informado é positivo: ' + a : 'O valor informado é negativo: ' + a;
+
+console.log(res);
