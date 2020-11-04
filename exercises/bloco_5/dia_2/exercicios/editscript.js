4554a8457cd944b18621f25e8bfc45c1d12ffdef
victor718 <victorfelipeoliveira1@gmail.com>
2020-11-04 19:18:30 -0300

    removendo os dois ultimos elemento da lista

--- exercises/bloco_5/dia_2/exercicios/editscript.js
+++ exercises/bloco_5/dia_2/exercicios/editscript.js
@@ -16,10 +16,31 @@ function edit3() {
     mainContent.removeChild(leftContent);
 }
 
+function edit4() {
+    let rightContent = document.getElementsByClassName('right-content');
+    rightContent[0].style.marginRight = 'auto';
+}
+
+function edit5() {
+    let paiDoCenterContent = document.querySelector('.center-content').parentNode;
+    paiDoCenterContent.style.backgroundColor = 'green';
+}
+
+function edit6() {
+    let liS = document.getElementsByTagName('li');
+    let ul = document.getElementsByTagName('ul');
+    ul[0].removeChild(liS[9]);
+    ul[0].removeChild(liS[8]);
+}
+
+
 function main() {
     edit1();
     edit2();
     edit3();
+    edit4();
+    edit5();
+    edit6();
 }
 
 main();
