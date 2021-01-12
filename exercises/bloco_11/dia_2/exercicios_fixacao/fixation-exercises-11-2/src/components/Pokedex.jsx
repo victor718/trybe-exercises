5a4b065505bbdf5b14abb992b6a63edfeeca0ed9
victor718 <victorfelipeoliveira1@gmail.com>
2021-01-12 17:57:20 -0300

    alterado class Pokedex

--- exercises/bloco_11/dia_2/exercicios_fixacao/fixation-exercises-11-2/src/components/Pokedex.jsx
+++ exercises/bloco_11/dia_2/exercicios_fixacao/fixation-exercises-11-2/src/components/Pokedex.jsx
@@ -4,19 +4,22 @@ import Pokemon from './Pokemon';
 import './Pokedex.css';
 
 class Pokedex extends Component {
-  mountPokedex() {
-    return Data.map(pokemon => {
-        return <Pokemon key={pokemon.id} pokemon={pokemon} />
-      })
-  }
+  // mountPokedex() {
+  //   return Data.map(pokemon => {
+  //       return <Pokemon key={pokemon.id} pokemon={pokemon} />
+  //     })
+  // }
 
   render() {
+
     return [
       <div className="content-title">
         <h1 className="title-pokedex">Pokedex</h1>
       </div>,
       <div className="pokedex-content">
-        {this.mountPokedex()}
+        {Data.map(pokemon => (
+          <Pokemon key={pokemon.id} pokemon={pokemon}/>
+        ))}
       </div>
     ]
   }
