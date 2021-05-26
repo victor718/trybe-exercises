d8c5e9c9441a2dcfe0b7543469394c4f6ec1f255
victor718 <victorfelipeoliveira1@gmail.com>
2021-05-26 19:31:51 -0300

    at

--- exercises/modulo_des_back_end/bloco_26/dia_4/exercise/index.js
+++ exercises/modulo_des_back_end/bloco_26/dia_4/exercise/index.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+const simpsons = require('./readFileSimpsons');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +15,38 @@ app.post('/hello', (req, res) => {
   const { name } = req.body;
   console.log(name)
   res.send({ "message": `Hello ${ name }` });
+})
+
+app.post('/greetings', (req, res) => {
+  console.log(req.query);
+  const { age, name } = req.body;
+  if (Number(age) > 17) {
+    res.status(200).json({ "message": `Hello, ${name}` });
+  } else {
+    res.status(401).json({ "message": "Unauthorized" });
+  }
+});
+
+app.put('/users/:name/:age', (req, res) => {
+  const { name, age } = req.params;
+  res.send({ "message": `Seu nome é  ${ name } e  você tem ${ age } anos de idade` })
+})
+
+app.get('/simpsons',  (req, res, next) => {
+  simpsons.getAllSimpsons().then((resolve) => {
+    res.status(200).json(resolve);
+  })
+  .catch(next);
+})
+
+app.get('/simpsons/:id', (req, res, next) => {
+  const { id } = req.params;
+  simpsons.getSimpsonsId(id)
+    .then((result) => {
+      if (result) res.status(200).json(result);
+      res.status(404).send({message: "simpson not found"})
+    })
+    .catch(next);
 })
 
 app.listen(3000, () => console.log(`App ouvindo a porta 3000:`));
